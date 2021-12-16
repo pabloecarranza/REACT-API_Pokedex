44a65ab92f23712758cdf85e4fb39dfcad197fbf
pabloecarranza <63490339+pabloecarranza@users.noreply.github.com>
2021-12-16 14:13:25 -0300

    Update Pokemons.js

--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -1,15 +1,14 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import Pokemon from "./Pokemon";
 import CardPokemon from './CardPokemon'
-const Pokemons = ({allPokemons}) => {
+import { useAppContext } from '../store/Context'
 
-    const [state, setstate] = useState({
-        pokemon:{},
-        visible: false
-    })
-
+const Pokemons = () => {
+  const { allPokemons, loadMore, setLoadMore, filtrar, filteredPokes, createPokemonObject } = useAppContext()
+  const [busqueda, setBusqueda] = useState("");
+  const [state, setstate] = useState({pokemon:{},visible: false})
     
-  const filtrar = (terminoBusqueda) => {
+  const filtrarPoke = (terminoBusqueda) => {
     let resultadosBusqueda = allPokemons?.filter((elemento) => {
       if (
         elemento.name
@@ -24,34 +23,67 @@ const Pokemons = ({allPokemons}) => {
   };
 
 
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+
+    setLoadMore(data.next);
+
+   createPokemonObject(data.results);
+  };
+
+  const handleChange = (e) => {
+    setBusqueda(e.target.value);
+    filtrar(e.target.value);
+  };
 
     const handleClick = (a) => {
-        filtrar(a.target.alt)
+      filtrarPoke(a.target.alt)
     }
 
     const handleInvisible = () => {
         setstate({...state, visible: false})
     }
 
-    
+    useEffect(() => {
+      getAllPokemons();
+    }, []);
 
-    return <div>
-        {allPokemons?.map((pokemonStats, index) => (
-            <Pokemon
-              key={index}
-              id={pokemonStats.id}
-              image={pokemonStats.sprites.other.dream_world.front_default}
-              name={pokemonStats.name}
-              type={pokemonStats.types[0].type.name}
-              handleClick={handleClick}
-            />
-          ))}
-         {state.visible && <>
-            <CardPokemon pokemon={state.pokemon} handleInvisible={handleInvisible}/>
-
-          </>}
-        </div>
+    return (
+      <div className="app-contaner">
+      <div className="containerInput">
+        <input
+          className="inputSearch"
+          value={busqueda}
+          placeholder="Buscar Pokemon"
+          onChange={handleChange}
+        />
+      </div>
   
+      <div className="pokemon-container">
+        <div className="all-container">
+        {filteredPokes?.map((pokemonStats, index) => (
+              <Pokemon
+                key={index}
+                id={pokemonStats.id}
+                image={pokemonStats.sprites.other.dream_world.front_default}
+                name={pokemonStats.name}
+                type={pokemonStats.types[0].type.name}
+                handleClick={handleClick}
+              />
+            ))}
+           {state.visible && <>
+              <CardPokemon pokemon={state.pokemon} handleInvisible={handleInvisible}/>
+  
+            </>}
+        </div>
+        <button className="load-more" onClick={() => getAllPokemons()}>
+          Mostrar Mas
+        </button>
+      </div>
+    </div>
+    )
 }
 
 export default Pokemons
+
